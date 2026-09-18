ca8000f34a6f7f47bad8290b952fbd373e60844c
junseonhong <137370275+junseonhong@users.noreply.github.com>
2026-09-18 14:25:54 -0700

    exp3

--- Exp3/stims_exp3.js
+++ Exp3/stims_exp3.js
@@ -1,6 +1,7 @@
 window.EXP3_PRIMES = [
   {id: "P_S01",
     qp_position: "subject",
+    lexical_set: "artist_star_cut",
     image: "primes/subject_artist_star_color.png",
     inverse_sentence: "모든 화가가 별을 칠한 것은 아니다.",
     control_sentence: "세 명의 화가 중 두 명이 별을 칠했다."
@@ -8,13 +9,47 @@ window.EXP3_PRIMES = [
 
   {id: "P_S02",
     qp_position: "subject",
+    lexical_set: "gardner_flower_water",
     image: "primes/subject_gardener_flower_water.png",
     inverse_sentence: "모든 정원사가 꽃에 물을 준 것은 아니다.",
     control_sentence: "세 명의 정원사 중 두 명이 꽃에 물을 줬다."
   },
 
+  {id: "P_S03",
+    qp_position: "subject",
+    lexical_set: "monkey_apple_put",
+    image: "primes/subject_monkey_apple_put.png",
+    inverse_sentence: "모든 원숭이가 바구니에 사과를 넣은 것은 아니다.",
+    control_sentence: "세 마리의 원숭이 중 두 마리가 바구니에 사과를 넣었다."
+  },
+  
+  {id: "P_S04", 
+    qp_position: "subject",
+    lexical_set: "baby_block_put",
+    image: "primes/subject_baby_block_put.png",
+    inverse_sentence: "모든 아이가 블럭을 상자에 넣은 것은 아니다.",
+    control_sentence: "세 명의 아이 중 두 명이 블럭을 상자에 넣었다."
+  },
+
+  {id: "P_S05",
+    qp_position: "subject",
+    lexical_set: "waiter_mug_break",
+    image: "primes/subject_waiter_mug_break.png",
+    inverse_sentence: "모든 웨이터가 머그컵을 깬 것은 아니다.",
+    control_sentence: "세 명의 웨이터 중 두 명이 머그컵을 깼다."
+  },
+
+  {id: "P_S06",
+    qp_position: "subject",
+    lexical_set: "teacher_light_on",
+    image: "primes/subject_teacher_light_on.png",
+    inverse_sentence: "모든 선생님이 손전등을 켠 것은 아니다.",
+    control_sentence: "세 명의 선생님 중 두 명이 손전등을 켰다."
+  },
+
   {id: "P_O01",
     qp_position: "object",
+    lexical_set: "bear_fish_eat",
     image: "primes/object_bear_fish_eat.png",
     inverse_sentence: "곰이 모든 물고기를 먹은 것은 아니다.",
     control_sentence: "곰이 세 마리의 물고기 중 두 마리를 먹었다."
@@ -22,34 +57,127 @@ window.EXP3_PRIMES = [
 
   {id: "P_O02",
     qp_position: "object",
+    lexical_set: "police_door_open",
     image: "primes/object_police_door_open.png",
     inverse_sentence: "경찰이 모든 문을 연 것은 아니다.",
     control_sentence: "경찰이 세 개의 문 중 두 개를 열었다."
+  },
+
+  {id: "P_O03",
+    qp_position: "object",
+    lexical_set: "chef_dish_break",
+    image: "primes/object_chef_dish_break.png",
+    inverse_sentence: "요리사가 모든 접시를 깬 것은 아니다.",
+    control_sentence: "요리사가 세 개의 접시 중 두 개를 깼다."
+  },
+
+  {id: "P_O04",
+    qp_position: "object",
+    lexical_set: "monkey_apple_put",
+    image: "primes/object_monkey_apple_put.png",
+    inverse_sentence: "원숭이가 모든 사과를 바구니에 넣은 것은 아니다.",
+    control_sentence: "원숭이가 세 개의 사과 중 두 개를 바구니에 넣었다."
+  },
+
+  {id: "P_O05",
+    qp_position: "object",
+    lexical_set: "student_f_paper_tear",
+    image: "primes/object_student_f_paper_tear.png",
+    inverse_sentence: "학생이 모든 종이를 찢은 것은 아니다.",
+    control_sentence: "학생이 종이 세 장 중 두 장을 찢었다."
+  },
+
+  {id: "P_O06",
+    qp_position: "object",
+    lexical_set: "waiter_coffee_fill",
+    image: "primes/object_waiter_coffee_fill.png",
+    inverse_sentence: "웨이터가 모든 커피 잔을 채운 것은 아니다.",
+    control_sentence: "웨이터가 커피 잔 세개 중 두개를 채웠다."
   },
 ];
 
 window.EXP3_TARGETS = [
   {id: "T_S01",
     qp_position: "subject",
-    image: "targets/subject_student_pencil_break.png",
-    sentence: "모든 힉생이 연필을 부러뜨리지 않았다."
+    lexical_set: "student_m_pencil_break",
+    image: "targets/subject_student_m_pencil_break.png",
+    sentence: "모든 학생이 연필을 부러뜨리지 않았다."
   },
 
   {id: "T_S02",
     qp_position: "subject",
+    lexical_set: "chef_carrot_cut",
     image: "targets/subject_chef_carrot_cut.png",
     sentence: "모든 요리사가 당근을 썰지 않았다."
   },
 
+  {id: "T_S03",
+    qp_position: "subject",
+    lexical_set: "student_m_book_tear",
+    image: "targets/subject_student_m_book_tear.png",
+    sentence: "모든 학생이 책을 찢지 않았다."
+  },
+
+  {id: "T_S04",
+    qp_position: "subject",
+    lexical_set: "child_apple_eat",
+    image: "targets/subject_child_apple_eat.png",
+    sentence: "모든 아이가 사과를 먹지 않았다."
+  },
+
+  {id: "T_S05",
+    qp_position: "subject",
+    lexical_set: "police_box_open",
+    image: "targets/subject_police_box_open.png",
+    sentence: "모든 경찰이 상자를 열지 않았다."
+  },
+
+  {id: "T_S06",
+    qp_position: "subject",
+    lexical_set: "waiter_wine_fill",
+    image: "targets/subject_waiter_wine_fill.png",
+    sentence: "모든 웨이터가 와인 잔을 채우지 않았다."
+  },
+
   {id: "T_O01",
     qp_position: "object",
-    image: "targets/object_waiter_wine_fill.png",
-    sentence: "웨이터가 모든 와인 잔을 채우지 않았다."
+    lexical_set: "student_f_newspaper_roll",
+    image: "targets/object_student_f_newspaper_roll.png", 
+    sentence: "학생이 모든 신문을 말지 않았다."
   },
 
   {id: "T_O02",
     qp_position: "object",
-    image: "targets/object_teacher_light_on.png",
+    lexical_set: "teacher_lamp_on",
+    image: "targets/object_teacher_lamp_on.png",
     sentence: "선생님이 모든 전등을 켜지 않았다."
+  },
+
+  {id: "T_O03",
+    qp_position: "object",
+    lexical_set: "robot_block_put",
+    image: "targets/object_robot_block_put.png",
+    sentence: "로봇이 모든 블럭을 원 안에 넣지 않았다."
+  },
+
+  {id: "T_O04",
+    qp_position: "object",
+    lexical_set: "chef_cheese_cut",
+    image: "targets/object_chef_cheese_cut.png",
+    sentence: "요리사가 모든 치즈를 자르지 않았다."
+  },
+
+  {id: "T_O05",
+    qp_position: "object",
+    lexical_set: "student_m_paper_fold",
+    image: "targets/object_student_m_paper_fold.png",
+    sentence: "학생이 모든 종이를 접지 않았다."
+  },
+
+  {id: "T_O06",
+    qp_position: "object",
+    lexical_set: "gardner_pot_water",
+    image: "targets/object_gardner_pot_water.png",
+    sentence: "정원사가 모든 화분에 물을 주지 않았다."
   },
 ];
